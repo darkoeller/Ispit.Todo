cb48b32ba63c7e85b692707d8a436e9c2a2f09dd
darkoeller <darko.eller@gmail.com>
2023-12-10 22:10:47 +0100

    dotjerivanje

--- Ispit.Todo/ViewModels/UpdateUserViewModel.cs
+++ Ispit.Todo/ViewModels/UpdateUserViewModel.cs
@@ -5,17 +5,17 @@ namespace Ispit.Todo.ViewModels;
 
 public class UpdateUserViewModel
 {
-	[StringLength(50)]
+	[StringLength(50, ErrorMessage = "Ne može biti duže od 50 znakova")]
 	public string? Ime { get; set; } = string.Empty;
-	[StringLength(50)]
+	[StringLength(50, ErrorMessage = "Ne može biti duže od 50 znakova")]
 	public string? Prezime { get; set; } = string.Empty;
-	[StringLength(250)]
+	[StringLength(250, ErrorMessage = "Ne može biti duže od 250 znakova")]
 	public string? Adresa { get; set; } = string.Empty;
-	[StringLength(50)]
+	[StringLength(50, ErrorMessage = "Ne može biti duže od 50 znakova")]
 	public string? Grad { get; set; } = string.Empty;
 	[StringLength(5, ErrorMessage = "Mora biti 5 znakova.")]
 	public string? PostanskiBroj { get; set; }
-	[StringLength(50)]
+	[StringLength(50, ErrorMessage = "Ne može biti duže od 50 znakova")]
 	public string? Drzava { get; set; } = string.Empty;
 
 	public IEnumerable<TaskItem>? TaskItems { get; set; }
