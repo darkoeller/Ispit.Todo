362c402c93b227b4d9608f3dc9ed2a9372895535
darkoeller <darko.eller@gmail.com>
2023-12-10 18:56:17 +0100

    SkrivaCheckBox

--- Ispit.Todo/wwwroot/js/site.js
+++ Ispit.Todo/wwwroot/js/site.js
@@ -2,6 +2,7 @@
   $('#myCheckbox').on('change',function () {
     if (this.checked) {
       var taskId = $(this).data('task-id');
+      $('#myCheckbox').hide();
       $.post('/TaskItems/UpdateTaskStatus', { id: taskId }, function (data) {
        
       });
